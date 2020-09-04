63090729c2b98ceebb739d60346db8019b9f48dd
Alex Gonzalez <alejandro.g.carrillo@hotmail.com>
2020-09-03 20:22:48 -0500

    App FactMe Finished

--- FactMe!/FactMe!/GameViewController.swift
+++ FactMe!/FactMe!/GameViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class GameViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var trueButton: UIButton!
@@ -18,15 +19,39 @@ class GameViewController: UIViewController {
     var game: QMeeGame = QMeeGame()
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         
+        pointLabel.text = "--"
+        let starterImage = UIImage(named: "2")
+        questionImageView.image = starterImage
+        questionLabel.text = "Tap the true button to start a new game!"
+        
+        falseButton.isEnabled = false
     }
     
     @IBAction func trueButtonDidTap() {
-        
+        if pointLabel.text == "--" {
+            game.point = 0
+            falseButton.isEnabled = true
+        } else {
+            game.point += 1
+        }
+        updateUI()
     }
     
     @IBAction func falseButtonDidTap() {
-        
+        game.point -= 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        let nextQuestion = game.getNextQuestion()
+        let questionImage = game.getQuestionImageName()
+        let image = UIImage(named: questionImage)
+        questionImageView.image = image
+        questionLabel.text = nextQuestion
+        pointLabel.text = "\(game.point)"
     }
 }
+
 
