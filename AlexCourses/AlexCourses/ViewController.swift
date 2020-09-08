63f3dbd0d05bb952472254887df551887e7865c6
Alex Gonzalez <alejandro.g.carrillo@hotmail.com>
2020-09-07 22:26:11 -0500

    Starting online course app

---
+++ AlexCourses/AlexCourses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlexCourses
+//
+//  Created by Alejandro Daniel Gonzalez on 9/6/20.
+//  Copyright © 2020 Alejandro Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
